b21af9421c21399eb409959768a285d2b7aa3a3a
Luan4560 <luan.nascimento11@outlook.com>
2020-05-07 23:32:20 -0300

    UX Concepts on the Form added

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,110 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import {
+  Container,
+  Title,
+  // ForgotPassword,
+  // ForgotPasswordText,
+  BackToSignUp,
+  BackToSignUpText,
+} from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const formRef = useRef<FormHandles>(null);
+  const navigation = useNavigation();
+
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordIputRef = useRef<TextInput>(null);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title> Crie sua conta </Title>
+            </View>
+            <Form
+              ref={formRef}
+              onSubmit={data => {
+                console.log(data);
+              }}
+            >
+              <Input
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="Nome"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  emailInputRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={emailInputRef}
+                keyboardType="email-address"
+                autoCorrect={false}
+                autoCapitalize="none"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordIputRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={passwordIputRef}
+                secureTextEntry
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                textContentType="newPassword"
+                returnKeyType="send"
+                onSubmitEditing={() => formRef.current?.submitForm()}
+              />
+
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Entrar
+              </Button>
+            </Form>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignUp onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignUpText>Voltar para logon </BackToSignUpText>
+      </BackToSignUp>
+    </>
+  );
 };
 
 export default SignUp;
